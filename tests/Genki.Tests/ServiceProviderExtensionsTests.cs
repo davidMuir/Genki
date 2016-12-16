a8e580042738fc6d3e604a315941982077652f34
David Muir <hey@davidmuir.co>
2016-12-16 22:26:33 +0000

    Ensure that we can add and retrieve multiple steps

--- tests/Genki.Tests/ServiceProviderExtensionsTests.cs
+++ tests/Genki.Tests/ServiceProviderExtensionsTests.cs
@@ -26,5 +26,31 @@ namespace Genki.Tests
             // Check that it's the correct step
             Assert.True(steps.Single().Name == "TestStep");
         }
+
+        [Fact]
+        public void GetHealthCheckSteps_Will_ReturnMultipleStepsWhenAvailable()
+        {
+            // new up a servicecollection so that we can build
+            // a service provider to retrieve our step from
+            var serviceCollection = new ServiceCollection();
+
+            // Add our health check
+            serviceCollection.AddHealthCheckStep<TestStep>();
+            serviceCollection.AddHealthCheckStep<TestStep2>();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();  
+
+
+            // Try and get our step which shouldn't fail since 
+            // we've added the type
+            var steps = serviceProvider.GetHealthCheckSteps();
+
+            // Check that it's the correct step
+            Assert.True(steps.Count() == 2);
+
+            // Check that we have the steps we expect
+            Assert.True(steps.Count(s => s.Name == "TestStep") == 1);
+            Assert.True(steps.Count(s => s.Name == "TestStep2") == 1);
+        }
     }
 }
